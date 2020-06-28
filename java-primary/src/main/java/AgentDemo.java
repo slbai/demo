601248a66f5eaf8e6cf098b74fbff7a9749fa379
shilin.bai <shilin.bai@beibei.com>
2020-06-28 14:15:15 +0800

    java agent

---
+++ java-primary/src/main/java/AgentDemo.java
@@ -0,0 +1,15 @@
+import java.lang.instrument.Instrumentation;
+
+public class AgentDemo {
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println("=========premain方法执行1========");
+        System.out.println(agentArgs);
+    }
+
+
+    public static void premain(String agentArgs) {
+        System.out.println("=========premain方法执行2========");
+        System.out.println(agentArgs);
+    }
+}
---
+++ java-primary/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    /**
+     * project struct里选择artifact, manifest文件必须是src的子路径
+     * -javaagent:E:\workspace\java\demo\out\artifacts\java_primary_jar\java-primary.jar=Hello
+     * -javaagent:E:\workspace\java\demo\out\artifacts\java_primary_jar\java-primary.jar=World
+     */
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
+}
